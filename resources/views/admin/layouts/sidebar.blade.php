b71f571b107b173c946ce60e34501b3e09eec45a
maulana677 <maull0776@gmail.com>
2024-04-18 15:11:45 +0700

    fix: update sidebar 2

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -41,7 +41,7 @@
             <a href="index.html">Creatifolio</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="index.html">CF</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
